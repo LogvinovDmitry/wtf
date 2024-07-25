9581d9ebb9140c8ce360587c86135f34bf1d49a3
LogvinovDmitry <112734820+LogvinovDmitry@users.noreply.github.com>
2024-07-25 18:09:50 +0300

    Added Liquibase and made test table in database. (#6)
    Todo:
    - delete hardcode in application.yaml (move it to a separate file)
    - create real table for entity
    
    docker-compose and application.yaml I send to public chat in Slack*

--- src/main/java/com/wtf/WtfApplication.java
+++ src/main/java/com/wtf/WtfApplication.java
@@ -1,18 +1,15 @@
 package com.wtf;
 
-import com.uwetrottmann.tmdb2.Tmdb;
-import com.uwetrottmann.tmdb2.entities.*;
-
-import com.uwetrottmann.tmdb2.enumerations.MediaType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-
-import java.io.IOException;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.wtf")
+@EnableJpaRepositories
+
+
 public class WtfApplication {
 
     public static void main(String[] args) {
